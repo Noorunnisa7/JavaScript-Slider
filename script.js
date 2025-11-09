616d08b13eccf1de6bd4bba9914162e80a355cdd
noorunnisa7 <noorunnisa560@gmail.com>
2025-11-10 01:50:59 +0500

    updated

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-let slideImg = []; 
+let slideImg = [];
 
-slideImg[0] = new Image();
-slideImg[0].src  = "s1.jpg"
+slideImg[0] = new Image(); slideImg[0].src = "s1.jpg";
+slideImg[1] = new Image(); slideImg[1].src = "s2.jpg";
+slideImg[2] = new Image(); slideImg[2].src = "s3.jpg";
+slideImg[3] = new Image(); slideImg[3].src = "s2.jpg";
 
-slideImg[1] = new Image();
-slideImg[1].src  = "s2.jpg"
-
-slideImg[2] = new Image();
-slideImg[2].src  = "s3.jpg"
-
-slideImg[3] = new Image();
-slideImg[3].src  = "s2.jpg"
-
-console.log(slideImg.length)
-console.log(slideImg[0])
 let i = 0;
-function next(){
-    i++
-    if(i > slideImg.length-1){
-        i = 0
-    }
 
-    document.getElementById('slideShow').src = slideImg[i].src
+function changeImage(index) {
+    let img = document.getElementById('slideShow');
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        img.src = slideImg[index].src;
+        img.style.opacity = 1;
+    }, 300);
 }
-function previous(){
- i--
- if(i < 0){
-    i = slideImg.length - 1
- }
- document.getElementById('slideShow').src = slideImg[i].src
+
+function next() {
+    i++;
+    if (i > slideImg.length - 1) i = 0;
+    changeImage(i);
+}
+
+function previous() {
+    i--;
+    if (i < 0) i = slideImg.length - 1;
+    changeImage(i);
 }
